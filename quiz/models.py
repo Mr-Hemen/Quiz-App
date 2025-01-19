7dbb38fb0adece6c833777701a7ceedfeed2ce43
Hassan Rasoulpour <hassan.rasoulpour1@gmail.com>
2025-01-19 16:15:58 +0330

    Create quiz app model

--- quiz/models.py
+++ quiz/models.py
@@ -1,2 +1,35 @@
 from django.db import models
 
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    # Multiple choice answers
+    option_1 = models.CharField(max_length=255)
+    option_2 = models.CharField(max_length=255)
+    option_3 = models.CharField(max_length=255)
+    option_4 = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    corret_answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+
+
+class UserQuiz(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.quiz.title}"
